bc8a365c7e3ee3a93c7e1bbb53bb5e0a8e2f6367
beomho <beomho.kor@gmail.com>
2024-10-18 10:22:42 +0900

    .gradle

---
+++ src/main/java/com/app/altibase/mapper/AltibaseMapper.java
@@ -0,0 +1,15 @@
+package com.app.altibase.mapper;
+
+import java.util.List;
+
+import com.app.altibase.vo.AltibaseVo;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper       //Mybatis Mapper임을 명시 하는 어노테이션
+public interface AltibaseMapper {
+
+    //전체 리스트 조회
+    public List<AltibaseVo> list();
+    
+}
